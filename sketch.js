58e4466c99da8ff136d95cee1ff17cc5d9278be5
Rileyk2006 <smigui2006@gmail.com>
2025-10-03 16:03:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,9 +36,9 @@ function draw()
 			velocityZ = orientationZ - pRotationZ;
 
 			textSize(16);
-			text("X: " + orientationX.toFixed(1) + "°", width/2, height/2);
-			text("Y: " + orientationY.toFixed(1) + "°", width/2, height/2);
-			text("Z: " + orientationZ.toFixed(1) + "°", width/2, height/2);
+			text("X: " + orientationX.toFixed(1) + "°", width/2, (height/2)+50);
+			text("Y: " + orientationY.toFixed(1) + "°", width/2, (height/2)+70);
+			text("Z: " + orientationZ.toFixed(1) + "°", width/2, (height/2)+90);
 		}
 
 }
