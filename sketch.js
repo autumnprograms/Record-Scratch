b327233bb903877c97d1c22994f5c1c66634d6bf
Rileyk2006 <smigui2006@gmail.com>
2025-10-03 15:39:03 -0400

    gyro test

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
 function setup() 
 {
+
+	x = rotationX	//Device tilt forward/backward	-180° to 180°
+	y = rotationY	//Device tilt left/right	-180° to 180°
+	z = rotationZ	//Device rotation around screen	-180° to 180°
 	createCanvas(400, 400);
+	//premade function that disables gdefault gestures
+	lockGestures();
+	enableGyroTap('Tap to enable motion sensors');
 }
 
 function draw()
-{
-
+{	
+	if (window.sensorsEnabled) {
+		text(x,50,50);
+		text(y,70,50);
+		text(z,90,50);
+	}
 }
